63ecc522b0cee054e39fc6095208c14390cba81c
IanSu18 <144109031+IanSu18@users.noreply.github.com>
2024-09-18 06:04:56 +0000

    Vailable code is updated

--- Sprint_Project_1.3.py
+++ Sprint_Project_1.3.py
@@ -1,7 +1,7 @@
 try:
     age=int(input("Please enter ur age: "))
-    
 except ValueError:
     print("This is not a valid reponse")
-
-    print("In one year, you will be ", age)
+    break 
+print(f"Your age is {age}")
+print(f"In one year, you will be {age+1})
